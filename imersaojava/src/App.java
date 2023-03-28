3260c229219f60ec8b4d0f0b2b84c89c81f044c2
CaGraciano <gracianocamila7@gmail.com>
2023-03-28 18:22:55 -0300

    desafios resolvidos

--- imersaojava/src/App.java
+++ imersaojava/src/App.java
@@ -1,3 +1,4 @@
+import java.io.File;
 import java.io.InputStream;
 import java.net.URI;
 import java.net.URL;
@@ -25,6 +26,9 @@ public class App {
         var parser = new JsonParser();
         List<Map<String, String>> listaDeFilmes = parser.parse(body);
 
+        var diretorio = new File("../figurinhas/");
+        diretorio.mkdir();
+
         // exibir e manipular os dados 
         var geradora = new GeradoraDeFigurinhas();
         for (Map<String,String> filme : listaDeFilmes) {
@@ -33,7 +37,7 @@ public class App {
             String titulo = filme.get("title");
 
             InputStream inputStream = new URL(urlImagem).openStream();
-            String nomeArquivo = titulo + ".png";
+            String nomeArquivo = "../figurinhas/" + titulo + ".png";
 
             geradora.cria(inputStream, nomeArquivo);
 
