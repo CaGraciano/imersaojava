0297d4b825c2e167c2f0a18715bc6d7169c65695
CaGraciano <gracianocamila7@gmail.com>
2023-03-29 20:36:01 -0300

    aula3 e desafios

--- imersaojava/src/GeradoraDeFigurinhas.java
+++ imersaojava/src/GeradoraDeFigurinhas.java
@@ -1,8 +1,6 @@
 import java.awt.Color;
 import java.awt.Font;
-import java.awt.FontMetrics;
 import java.awt.Graphics2D;
-import java.awt.geom.Rectangle2D;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.InputStream;
@@ -33,17 +31,12 @@ public class GeradoraDeFigurinhas {
         graphics.drawImage(imagemOriginal, 0, 0, null);
 
         // configurar a fonte
-        var fonte = new Font("Impact", Font.BOLD, 80);
+        var fonte = new Font(Font.SANS_SERIF, Font.BOLD, 64);
         graphics.setColor(Color.YELLOW);
         graphics.setFont(fonte);
 
         // escrever uma frase na nova imagem
-        String texto = "TOPZERA";
-        FontMetrics fontMetrics = graphics.getFontMetrics(); 
-        Rectangle2D retangulo = fontMetrics.getStringBounds(texto, graphics);
-        int larguraTexto = (int) retangulo.getWidth();
-        int posicaoTextoX = (largura - larguraTexto) /2;
-        graphics.drawString(texto, posicaoTextoX, novaAltura - 100);
+        graphics.drawString("TOPZERA", 100, novaAltura - 100);
 
         // escrever a nova imagem em um arquivo
         ImageIO.write(novaImagem, "png", new File(nomeArquivo));
